2b590e530c31f030911137409e1d44224084ee5b
pritom-07 <pritomhaven@gmail.com>
2021-09-09 16:34:02 +0600

    team slick responsive and preloader

--- js/custom.js
+++ js/custom.js
@@ -31,9 +31,44 @@ $(function(){
 
     $('.team-slider').slick({
         arrows:false,
-        slidesToShow:4,
-        slidesToScroll:1,
-    });
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: false,
+              dots: false,
+              arrows:false,
+              autoplay:true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              autoplay:true,
+              
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              autoplay:true,
+              
+            }
+          }
+          
+        ]
+      });
 
 
 
