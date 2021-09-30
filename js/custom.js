58c9db48d33c844982f1e8b08dc2a08b9aa9214d
pritom-07 <pritomhaven@gmail.com>
2021-09-30 18:07:42 +0600

    fully responsive

--- js/custom.js
+++ js/custom.js
@@ -1,202 +1,220 @@
-$(function(){
+$(function () {
 
-    // preloader
-    $(window).on('load',function(){
-        $(".preloader").addClass("complete")
-    })
+  // preloader
+  $(window).on('load', function () {
+    $(".preloader").addClass("complete")
+  })
 
-    // navbar click active
-    var removecls = $('.nav-link').click(function(){
+  // navbar click active
+  var removecls = $('.nav-link').click(function () {
 
-        removecls.removeClass('active');
-        $(this).addClass('active');
-    });
-
-
-// back to top button
- 
-$(window).scroll(function(){
-
-  var scroll =$(window).scrollTop();
-  
-  if(scroll > 50){
-    $(".back-to").addClass("back-to-top");
-    $("nav").addClass("nav-fixed");
-    $(".navbar-brand").removeClass("logo-padding");
-  }
-
-  else{
-    $(".back-to").removeClass("back-to-top");
-    $("nav").removeClass("nav-fixed");
-    $(".navbar-brand").addClass("logo-padding");
-
-  }
-
-
-  // nav active
-  var remove = $('.nav-link');
-  if(scroll < 20){
-    $(remove).removeClass("active");
-    $(".home").addClass("active");
-    
-  }
-
-
-});
-
-$(".back-to").click(function(){
-  $("html,body").animate({
-    scrollTop:0,
-  });
-  
-
-
-});
-
-
-
-
-    // banner slider 
-    $('.banner-slider').slick({
-        arrows:false,
-        dots:true,
-        autoplay:true,
-        autoplaySpeed:2000,
-    });
-
-
-    // venobox video play
-    $('.venobox').venobox({
-        closeBackground:'#e23e38',
-        spinner:'wave',
-        
-    }); 
-
-    // team slider 
-
-    $('.team-slider').slick({
-        arrows:false,
-        dots: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              infinite: false,
-              dots: false,
-              arrows:false,
-              autoplay:true,
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              autoplay:true,
-              
-            }
-          },
-          
-          
-        ]
-      });
-      
-
-   // custom cursor
-   
-    var kursorx = new kursor({
-
-    type: 1,
-    color: '#e23e38',
-
-    });
-
-
-    // testimonial slider
-
-    $('.testimonial-slider').slick({
-      arrows:false,
-      dots: true,
-      infinite: true,
-      autoplay:true,
-      speed: 300,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      responsive: [
-
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true,
-            arrows:false,
-            autoplay:true,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay:true,
-            dots: true,
-            
-          }
-        },
-        
-        
-      ]
-    });
-
-
-    // counter in funfact
-    $('.counter').counterUp({
-      delay: 10,
-      time: 1000,
+    removecls.removeClass('active');
+    $(this).addClass('active');
   });
 
 
-  // brand slider
-  $('.brand-slider').slick({
-    slidesToShow:5,
-    slidesToScroll:1,
-    prevArrow:'<i class="slick-prev fas fa-chevron-left common-arr left-arrow"></i>',
-    nextArrow:'<i class="slick-prev fas fa-chevron-right common-arr right-arrow"></i>',
-    centerMode:true,
-    centerPadding:'0px',
-    autoplay:true,
-    autoplaySpeed:2000,
-    responsive: [
-      {
+  // back to top button
+
+  $(window).scroll(function () {
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 50) {
+      $(".back-to").addClass("back-to-top");
+      $("nav").addClass("nav-fixed");
+      $(".navbar-brand").removeClass("logo-padding");
+    } else {
+      $(".back-to").removeClass("back-to-top");
+      $("nav").removeClass("nav-fixed");
+      $(".navbar-brand").addClass("logo-padding");
+
+    }
+
+
+    // nav active
+    var remove = $('.nav-link');
+    if (scroll < 20) {
+      $(remove).removeClass("active");
+      $(".home").addClass("active");
+
+    }
+
+
+  });
+
+  $(".back-to").click(function () {
+    $("html,body").animate({
+      scrollTop: 0,
+    });
+
+
+
+  });
+
+
+
+
+  // banner slider 
+  $('.banner-slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+
+  // venobox video play
+  $('.venobox').venobox({
+    closeBackground: '#e23e38',
+    spinner: 'wave',
+
+  });
+
+  // team slider 
+
+  $('.team-slider').slick({
+    arrows: false,
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
         breakpoint: 992,
         settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '0px',
-          autoplay:true,
-          autoplaySpeed:2000,
-          slidesToShow: 3,
-          slidesToScroll:1,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+          dots: false,
+          arrows: false,
+          autoplay: true,
         }
       },
       {
         breakpoint: 768,
         settings: {
-          arrows: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplay: true,
+
+        }
+      },
+
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+          autoplay: true,
+
+        }
+      },
+
+
+    ]
+  });
+
+
+  // custom cursor
+
+  var kursorx = new kursor({
+
+    type: 1,
+    color: '#e23e38',
+
+  });
+
+
+  // testimonial slider
+
+  $('.testimonial-slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+          autoplay: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          dots: true,
+
+        }
+      },
+
+
+    ]
+  });
+
+
+  // counter in funfact
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000,
+  });
+
+
+  // brand slider
+  $('.brand-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: '<i class="slick-prev fas fa-chevron-left common-arr left-arrow"></i>',
+    nextArrow: '<i class="slick-prev fas fa-chevron-right common-arr right-arrow"></i>',
+    centerMode: true,
+    centerPadding: '0px',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+          arrows: false,
           centerMode: true,
           centerPadding: '0px',
-          autoplay:true,
-          autoplaySpeed:2000,
+          autoplay: true,
+          autoplaySpeed: 2000,
           slidesToShow: 3,
-          slidesToScroll:1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          autoplay: true,
+          autoplaySpeed: 2000,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          autoplay: true,
+          autoplaySpeed: 2000,
+          slidesToShow: 3,
+          slidesToScroll: 1,
         }
       }
     ]
@@ -205,7 +223,7 @@ $(".back-to").click(function(){
 
 
 
-    
+
 
 
 
